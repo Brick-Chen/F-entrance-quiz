ffd71f1671f0c16928c5fdeb5326e64f956c9be4
YumingChen <mathcym@outlook.com>
2020-10-27 23:26:00 +0800

    [yuming chen]: implement group students.

--- src/App/App.js
+++ src/App/App.js
@@ -8,6 +8,8 @@ class App extends Component {
     super(props);
     this.state = {
       students: [],
+      group: [],
+      showGroup: false,
     };
   }
 
@@ -28,10 +30,54 @@ class App extends Component {
       });
   };
 
+  getGroupStudents = () => {
+    http
+      .get('/group-students')
+      .then((res) => {
+        this.setState({
+          showGroup: true,
+          group: this.GroupStudents(res.data),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  GroupStudents = (data) => {
+    const num = parseInt(data.length / 6, 10);
+    const remain = data.length % 6;
+    const groupStu = [];
+    for (let i = 0; i < num * 6; i += num) {
+      groupStu.push(data.slice(i, i + num));
+    }
+    for (let j = 0; j < remain; j += 1) {
+      groupStu[j].push(data[6 * num + j]);
+    }
+    return groupStu;
+  };
+
   render() {
     return (
       <div data-testid="app" className="App">
         <h2>分组列表</h2>
+        <button type="button" className="group-student" onClick={this.getGroupStudents}>
+          分组学员
+        </button>
+        {this.state.showGroup ? (
+          <div>
+            {this.state.group.map((item, index) => (
+              <div>
+                <div>第{index + 1}组</div>
+                {item.map((student) => (
+                  <Student key={student.id} id={student.id} name={student.name} />
+                ))}
+              </div>
+            ))}
+          </div>
+        ) : (
+          ''
+        )}
         <h2>学员列表</h2>
         <div className="students-list">
           {this.state.students.map((student) => (
